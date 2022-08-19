ceed8719cedcc15d22c3e8d0d29c0442103852cf
xisdenos <lucaspinto.andrade@gmail.com>
2022-08-19 16:21:51 -0300

    first commit

---
+++ GoReceitas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoReceitas
+//
+//  Created by Lucas Pinto on 19/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
