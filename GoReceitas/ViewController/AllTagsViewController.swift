6893671c9cef3a3ce2609be14edefb54071ff3c5
igor05k <igormedfer@gmail.com>
2022-09-19 18:01:53 -0300

    added new assets

--- GoReceitas/ViewController/AllTagsViewController.swift
+++ GoReceitas/ViewController/AllTagsViewController.swift
@@ -2,14 +2,17 @@ import UIKit
 
 class AllTagsViewController: UIViewController {
     private var tags: [AllTagsModel] = [
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
-        .init(image: "lasanha", title: "Especiais", subtitle: "nada saudável"),
+        .init(image: "japanese", title: "Japanese", subtitle: "Cuisine"),
+        .init(image: "healthy", title: "Healthy", subtitle: "Dietary"),
+        .init(image: "snacks", title: "Snacks", subtitle: "Meal"),
+        .init(image: "indian", title: "Indian", subtitle: "Cuisine"), 
+        .init(image: "taiwanese", title: "Taiwanese", subtitle: "Cuisine"),
+        .init(image: "italian", title: "Italian", subtitle: "Cuisine"),
+        .init(image: "middle-eastern", title: "Middle Eastern", subtitle: "Cuisine"),
+        .init(image: "breakfast", title: "Breakfast", subtitle: "Meal"),
+        .init(image: "bbq", title: "Barbecue", subtitle: "BBQ"),
+        .init(image: "vegetarian", title: "Vegetarian", subtitle: "Dietary"),
+         
     ]
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,9 @@ extension AllTagsViewController: UICollectionViewDelegate, UICollectionViewDataS
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let tagsCell = collectionView.dequeueReusableCell(withReuseIdentifier: AllTagsViewCell.identifier, for: indexPath) as! AllTagsViewCell
         tagsCell.setup(tagInfo: tags[indexPath.row])
+        tagsCell.layer.borderWidth = 2
+        tagsCell.layer.borderColor = CGColor(red: 73 / 255, green: 0 / 255, blue: 119 / 255, alpha: 1)
+        tagsCell.cornerRadius = 10
         return tagsCell
     }
     
