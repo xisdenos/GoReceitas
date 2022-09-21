485e6667f84ea5241719cdde0a7dfe9eadde6aa9
igor05k <igormedfer@gmail.com>
2022-09-20 23:41:56 -0300

    added enums folder

---
+++ GoReceitas/Enums/Sections.swift
@@ -0,0 +1,40 @@
+//
+//  Sections.swift
+//  GoReceitas
+//
+//  Created by Igor Fernandes on 06/09/22.
+//
+
+import Foundation
+
+enum Sections {
+    case categories([CellsInfoSections])
+    case tryItOut([CellsInfoSections])
+    case popular([CellsInfoSections])
+    
+    var items: [CellsInfoSections] {
+        switch self {
+        case .categories(let array):
+            return array
+        case .tryItOut(let array):
+            return array
+        case .popular(let array):
+            return array
+        }
+    }
+    
+    var count: Int {
+        return items.count
+    }
+    
+    var title: String {
+        switch self {
+        case .categories:
+            return "Categorias"
+        case .tryItOut:
+            return "Experimente"
+        case .popular:
+            return "Popular"
+        }
+    }
+}
