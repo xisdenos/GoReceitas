35abf9699ccba47d9c8a37ee660196ef8df9a154
Igor Fernandes <igormedfer@gmail.com>
2022-10-03 19:03:52 -0300

    added search view controller

---
+++ GoReceitas/ViewController/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  GoReceitas
+//
+//  Created by Igor Fernandes on 03/10/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
