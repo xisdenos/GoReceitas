b18a64858da3e868a0405fc8932dab944644751a
Igor Fernandes <igormedfer@gmail.com>
2022-10-16 10:51:26 -0300

    user created with email and password

--- GoReceitas/ViewController/Login/LoginViewController.swift
+++ GoReceitas/ViewController/Login/LoginViewController.swift
@@ -13,9 +13,27 @@ import GoogleSignIn
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var googleSignInButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password) { authDataResult, error in
+            guard error == nil, let user = authDataResult else {
+                return
+            }
+            let result = user.user
+            print("user logged in succesfully \(result)")
+        }
+    }
+    
     
     @IBAction func tappedGoogleSignInButton(_ sender: UIButton) {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
--- GoReceitas/ViewController/Register/RegisterViewController.swift
+++ GoReceitas/ViewController/Register/RegisterViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func registerButton(_ sender: UIButton) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { authDataResult, error in
+            guard error == nil, let user = authDataResult else {
+                print("something went wrong while creating a new account.")
+                return
+            }
+            
+            let result = user.user
+            print(result)
+        }
+    }
 }
