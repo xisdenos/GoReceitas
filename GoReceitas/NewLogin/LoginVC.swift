220e3d9177aef1255a4f7310adf26d0555264818
FranklinSolano <franklinsolano40@gmail.com>
2022-10-21 14:49:44 -0300

    Feito logica e reajustes das telas de login,cadastro e esqueceu a senha

--- GoReceitas/NewLogin/LoginVC.swift
+++ GoReceitas/NewLogin/LoginVC.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import  Firebase
+import GoogleSignIn
+import FirebaseAuth
 
 class LoginVC: UIViewController {
     
@@ -42,14 +45,19 @@ class LoginVC: UIViewController {
     
     @IBOutlet weak var imageLogoFundo: UIImageView!
     
+    var auth:Auth?
+    var alert: AlertController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        alert = AlertController(controller: self)
+        self.auth = Auth.auth()
         configCaracteristicas()
         JaTemCadastro()
-
+        
     }
     
-
+    
     func configCaracteristicas(){
         
         goLabel.text = "Go"
@@ -67,20 +75,25 @@ class LoginVC: UIViewController {
         emailLabel.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         
         textFieldEmail.placeholder = "Digite seu E-mail:"
+        textFieldEmail.keyboardType = .emailAddress
+        textFieldEmail.delegate = self
         
         senhaLabel.text = "Senha"
         senhaLabel.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
         
         textFieldSenha.placeholder = "Digite sua Senha"
-        
-        esqueceuSenhaButton.setTitle("Esqueceu senha?", for: .normal)
+        textFieldSenha.isSecureTextEntry = true
+        textFieldSenha.delegate = self
         
         
         loginButton.setTitle("Login", for: .normal)
         loginButton.backgroundColor = UIColor(red: 149/255, green: 1/255, blue: 239/255, alpha: 1)
+        loginButton.setTitleColor(.white.withAlphaComponent(0.4), for: .disabled)
         loginButton.setTitleColor(.white, for: .normal)
         loginButton.clipsToBounds = true
         loginButton.layer.cornerRadius = 10
+        loginButton.isEnabled = false
+        
         
         ouLabel.text = "OU"
         
@@ -95,34 +108,121 @@ class LoginVC: UIViewController {
         loginGoogleButton.setTitle("Entrar com o Google", for: .normal)
         loginGoogleButton.setTitleColor(.black, for: .normal)
         loginGoogleButton.backgroundColor = UIColor(red: 220/255, green: 220/255, blue: 221/255, alpha: 1)
+        loginGoogleButton.layer.cornerRadius = 10
+        loginGoogleButton.clipsToBounds = true
         
         imageLogoFundo.image = UIImage(named: "logoFundo")
         
     }
     
     func JaTemCadastro(){
-        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(ciColor: .black), .font: UIFont.systemFont(ofSize: 12, weight: .semibold)]
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(ciColor: .black), .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]
         let attributedTitle = NSMutableAttributedString(string: "Não tem uma conta? ", attributes: atts)
-        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), .font: UIFont.systemFont(ofSize: 12, weight: .semibold)]
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]
         attributedTitle.append(NSAttributedString(string: "Cadastre-se", attributes: boldAtts))
         fazerCadastroButton.setAttributedTitle(attributedTitle, for: .normal)
         fazerCadastroButton.setTitleColor(UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), for: .normal)    }
     
     
+    
+    
+    func validacaoTextField(){
+        if textFieldEmail.text != "" && textFieldSenha.text != ""{
+            loginButton.isEnabled = true
+        }else{
+            loginButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func tappedEsqueceuSenha(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "EsqueceuSenhaVC", bundle: nil).instantiateViewController(withIdentifier: "EsqueceuSenhaVC") as? EsqueceuSenhaVC
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
+    
     @IBAction func tappedLogin(_ sender: UIButton) {
+        
+        let email:String = self.textFieldEmail.text ?? ""
+        let senha:String = self.textFieldSenha.text ?? ""
+        
+        self.auth?.signIn(withEmail: email, password: senha,completion: { usuario, error in
+            if error != nil {
+                self.alert?.alertInformation(title: "Atenção", message: "Dados incorretos, tente novamente")
+                
+            }else{
+                if usuario == nil{
+                    self.alert?.alertInformation(title: "Atenção", message: "Tivemos um problema inesperado")                }else{
+                        self.alert?.alertInformation(title: "Login feito com sucesso", message: "")
+                }
+            }
+        })
+        
     }
     
     
     
     @IBAction func tappedLoginGoogle(_ sender: UIButton) {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+          guard error == nil else {
+              self.alert?.alertInformation(title: "Atenção", message: "Falha ao tentar realizar o login, Tente Novamente!")
+            return
+          }
+
+          guard
+            let authentication = user?.authentication,
+            let idToken = authentication.idToken
+          else {
+            return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: authentication.accessToken)
+
+            Auth.auth().signIn(with: credential) { dataResult, error in
+                guard error == nil else {
+                    
+                    self.alert?.alertInformation(title: "Atenção", message: "Falha ao tentar realizar o login, Tente Novamente!")
+                  return
+                    
+                }
+                print("Login com sucesso")
+            }
+        }
+        
+        
     }
     
     
     @IBAction func tappedFazerCadastroButton(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "RegisterVC", bundle: nil).instantiateViewController(withIdentifier: "RegisterVC") as? RegisterVC
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+        
     }
     
     
+    
+}
+
+extension LoginVC: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
     }
-   
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        validacaoTextField()
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
 
 
--- GoReceitas/NewRegister/RegisterVC.swift
+++ GoReceitas/NewRegister/RegisterVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterVC: UIViewController {
     
@@ -43,16 +44,20 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var logoImage: UIImageView!
     
     
+    var auth:Auth?
+    var alert: AlertController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        alert = AlertController(controller: self)
+        self.auth = Auth.auth()
         configCaracteres()
         backLogin()
     }
     
     func configCaracteres(){
         
-        voltarButton.setImage(UIImage(named: "back"), for: .normal)
+        voltarButton.setImage(UIImage(named: "back11"), for: .normal)
         
         goLabel.text = "Go"
         goLabel.textColor = UIColor(red: 149/255, green: 1/255, blue: 239/255, alpha: 1)
@@ -65,48 +70,85 @@ class RegisterVC: UIViewController {
         logoTopImage.image = UIImage(named: "logoTop")
         
         cadastrarLabel.text = "Cadastrar"
-        cadastrarLabel.font = UIFont.systemFont(ofSize: 40, weight: .semibold)
+        cadastrarLabel.font = UIFont.systemFont(ofSize: 28, weight: .semibold)
         
         nameLabel.text = "Nome"
         nameLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
         
         textFieldName.placeholder = "Digite seu Nome:"
+        textFieldName.delegate = self
         
         emailLabel.text = "Email"
         emailLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
         
         textFieldEmail.placeholder = "Digite seu Email:"
+        textFieldEmail.keyboardType = .emailAddress
+        textFieldEmail.delegate = self
         
         senhaLabel.text = "Senha"
         senhaLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
         
         textFieldSenha.placeholder = "Digite sua Senha:"
+        textFieldSenha.isSecureTextEntry = true
+        textFieldSenha.delegate = self
         
         confirmarSenhaLabel.text = "Confirmar a sua senha"
         confirmarSenhaLabel.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
         
         textFieldConfirmarSenha.placeholder = "Digite sua Senha Novamente:"
+        textFieldConfirmarSenha.isSecureTextEntry = true
+        textFieldConfirmarSenha.delegate = self
         
         cadastrarButton.setTitle("Cadastrar", for: .normal)
         cadastrarButton.backgroundColor = UIColor(red: 149/255, green: 1/255, blue: 239/255, alpha: 1)
+        cadastrarButton.setTitleColor(.white.withAlphaComponent(0.4), for: .disabled)
         cadastrarButton.setTitleColor(.white, for: .normal)
         cadastrarButton.layer.cornerRadius = 10
         cadastrarButton.clipsToBounds = true
+        cadastrarButton.isEnabled = false
+        
         
         logoImage.image = UIImage(named: "logoFundo")
         
     }
     func backLogin(){
-        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(ciColor: .black), .font: UIFont.systemFont(ofSize: 12, weight: .semibold)]
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(ciColor: .black), .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]
         let attributedTitle = NSMutableAttributedString(string: "Já tem conta? ", attributes: atts)
-        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), .font: UIFont.systemFont(ofSize: 12, weight: .semibold)]
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), .font: UIFont.systemFont(ofSize: 15, weight: .semibold)]
         attributedTitle.append(NSAttributedString(string: "Login", attributes: boldAtts))
         backLoginButton.setAttributedTitle(attributedTitle, for: .normal)
         backLoginButton.setTitleColor(UIColor(red: 101/255, green: 33/255, blue: 165/255, alpha: 1), for: .normal)    }
     
     
     
+    func validacaoTextField(){
+        let nome:String = self.textFieldName.text ?? ""
+        let email:String = self.textFieldEmail.text ?? ""
+        let senha:String = self.textFieldSenha.text ?? ""
+        let confirmarSenha = self.textFieldConfirmarSenha.text ?? ""
+        
+        if !nome.isEmpty && !email.isEmpty && !senha.isEmpty && !confirmarSenha.isEmpty {
+            self.configbuttonEnable(true)
+        }else{
+            self.configbuttonEnable(false)
+        }
+        
+    }
+    
+    private func configbuttonEnable(_ enable:Bool){
+        if enable{
+            self.cadastrarButton.setTitleColor(.white, for: .normal)
+            self.cadastrarButton.isEnabled = true
+        }else{
+            self.cadastrarButton.setTitleColor(.lightGray, for: .normal)
+            self.cadastrarButton.isEnabled = false
+        }
+    }
+    
+    
     @IBAction func tappedBackButton(_ sender: UIButton) {
+        
+        navigationController?.popViewController(animated: true)
         
     }
     
@@ -114,14 +156,53 @@ class RegisterVC: UIViewController {
     
     @IBAction func tappedCadastrarButton(_ sender: UIButton) {
         
+        let email:String = textFieldEmail.text ?? ""
+        let senha:String = textFieldSenha.text ?? ""
+        let confirmarSenha:String = textFieldConfirmarSenha.text ?? ""
+        
+        if senha == confirmarSenha {
+            self.auth?.createUser(withEmail: email, password: senha, completion: { [self] result, error in
+                
+                if error != nil{
+                    self.alert?.alertInformation(title: "Atenção", message: "Erro ao cadastrar, verifique os dados e tente novamente")
+                }else{
+                    self.alert?.alertInformation(title: "Parabens", message: "Usuario cadastrado com sucesso",completion: {
+                        self.navigationController?.popViewController(animated: true)
+                    })
+
+                }
+                
+                
+            })
+            
+        }else{
+            self.alert?.alertInformation(title: "Atenção", message: "Senhas Divergentes")        }
+        
+        
+        
+        
     }
     
     
     @IBAction func tappedJaTemLoginButton(_ sender: Any) {
-        
+        navigationController?.popViewController(animated: true)
     }
     
     
 }
 
 
+extension RegisterVC: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        validacaoTextField()
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
