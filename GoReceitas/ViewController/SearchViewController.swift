a540589ebc8a9b31907bbbdcd51bbff5a9ccb78f
Igor Fernandes <igormedfer@gmail.com>
2022-10-26 12:18:54 -0300

    added search bar controller and empty table view

--- GoReceitas/ViewController/SearchViewController.swift
+++ GoReceitas/ViewController/SearchViewController.swift
@@ -8,8 +8,42 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var containerSearchBar: UIView!
+    
+    lazy var searchController: UISearchController = {
+        let search = UISearchController(searchResultsController: nil)
+        search.searchBar.placeholder = "Food name..."
+        search.searchBar.searchBarStyle = .minimal
+//        search.searchResultsUpdater = self
+        return search
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Search"
         self.view.backgroundColor = .viewBackgroundColor
+        containerSearchBar.addSubview(searchController.searchBar)
+        configTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 }
