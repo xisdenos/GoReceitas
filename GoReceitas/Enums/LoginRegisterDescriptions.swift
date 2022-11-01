ec48250d0d1d51e8f47bce80319f7c64a8f57147
Igor Fernandes <igormedfer@gmail.com>
2022-11-01 14:06:32 -0300

    enum added to login and register

---
+++ GoReceitas/Enums/LoginRegisterDescriptions.swift
@@ -0,0 +1,29 @@
+//
+//  LoginDescriptions.swift
+//  GoReceitas
+//
+//  Created by Igor Fernandes on 01/11/22.
+//
+
+import Foundation
+
+enum LoginRegisterDescriptions: String {
+    case emailLabel = "E-mail"
+    case emailTextField = "Your e-mail..."
+    case passwordLabel = "Password"
+    case passwordTextField = "Your password..."
+    case signInLabel = "Login"
+    case signInWithGoogel = "Sign in with google"
+    case topLabel = "Receitas"
+    case goLabel = "Go"
+    case noAccountLabel = "Don't have an account? "
+    case signUpLabel = "Sign up"
+    case orLabel = "OR"
+    
+    // register
+    case nameLabel = "Your Name"
+    case registerLabel = "Register"
+    case confirmPasswordLabel = "Confirm your password"
+    case confirmPasswordTextField = "Your your password..."
+    case alreadyHaveAccount = "Already have an account? "
+}
---
+++ GoReceitas/Enums/RegisterDescriptions.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterDescriptions.swift
+//  GoReceitas
+//
+//  Created by Igor Fernandes on 01/11/22.
+//
+
+import Foundation
